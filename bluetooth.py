8413081d32c743a01a0325f5677126d640aefa60
Gary Zhou <garyzhou1201@gmail.com>
2025-12-04 00:02:41 -0500

    Clarify 's' command in bluetooth.py
    Added comment to clarify the 's' command functionality.

--- bluetooth.py
+++ bluetooth.py
@@ -17,7 +17,7 @@ except Exception as e:
 print("You can now type commands:")
 print("  l = turn left")
 print("  r = turn right")
-print("  s = straight")
+print("  s = straight") # move from 0
 print("  0 = calibrate")
 print("  q = quit")
 
